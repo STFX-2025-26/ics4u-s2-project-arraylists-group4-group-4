fd6f0e55b2b6304286b1e0cd03486d444874b287
s510024 <s510024@FXH-3972841347>
2026-02-23 13:16:02 -0500

    ArrayListMethods, create the methods in here.

---
+++ src/sandbox/ArrayListsMethods.java
@@ -0,0 +1,14 @@
+package sandbox;
+import java.util.ArrayList;	
+
+public class ArrayListsMethods {
+
+	/*
+	 * DESCRIPTION - List all the names
+	 * PARAMETERS - ArrayList
+	 * RETURN TYPE - String
+	 */
+	public static String listMethod(ArrayList x) {
+		return "s";
+	}
+}
